d97a313c772440b1a16153c123030fe67df4bf51
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-02 01:13:18 +0200

    initial commit - updating screens and game logic

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+
+const App: () => React$Node = () => {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      {content}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export default App;
