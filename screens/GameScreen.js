83e5375f6cce99b4bdd1bbccff4aecb58b434707
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-26 12:53:53 +0200

    Start using Dimensions api and flexible styling for smaller screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions,
 } from 'react-native';
 // import {Icon} from 'react-native-elements';
 import NumberContainer from '../components/NumberContainer';
@@ -79,6 +80,12 @@ const GameScreen = props => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+
+  if (Dimensions.get('window').width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -92,7 +99,7 @@ const GameScreen = props => {
           <Icon name="md-add" color="#fff" size={24} />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses.length - index),
@@ -118,13 +125,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%',
   },
   listContainer: {
     flex: 1,
     width: '60%',
+  },
+  listContainerBig: {
+    flex: 1,
+    width: '80%',
   },
   list: {
     flexGrow: 1,
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,6 +7,7 @@ import {
   Keyboard,
   Alert,
   Platform,
+  Dimensions,
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -114,8 +115,10 @@ const styles = StyleSheet.create({
     fontFamily: Platform.OS === 'ios' ? 'open-sans-bold' : 'OpenSans-Bold',
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    // maxWidth: '80%',
+    maxWidth: '95%',
+    minWidth: 300,
     alignItems: 'center',
   },
   buttonContainer: {
@@ -125,7 +128,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    // width: 100,
+    width: Dimensions.get('window').width / 4,
   },
   input: {
     width: 50,
