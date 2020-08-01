afdc1ee1f8e4ea48d15bdc591897924ec568cb01
Dannus90 <persson.daniel.1990@gmail.com>
2020-08-01 23:36:50 +0200

    App Finished, add Last Responsive Design

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -68,10 +68,10 @@ const App: () => React$Node = () => {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess a number" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 };
 
